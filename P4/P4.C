8a3ee41f87ab8e7e7ad5137b82920c49b2dc2d9d
Chinmay <sahoochinmay54@gmail.com>
2020-08-12 20:00:24 +0530

    P4 accept principal value , rate of interest and time then calculate the simple interest

---
+++ P4/P4.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    float P, R, T, SI;
+    printf("Enter the principal value:\n");
+    scanf("%f", &P);
+    printf("Enter rate of interest:\n");
+    scanf("%f", &R);
+    printf("Enter time in Year:\n");
+    scanf("%f", &T);
+    SI = (P * T * R) / 100;
+    printf("Simple Interest is %f", SI);
+    return 0;
+}
