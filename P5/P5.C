bf51a2696adbecb6615595aff1f7a04809adf940
Chinmay <sahoochinmay54@gmail.com>
2020-08-12 20:05:43 +0530

    Accept two number and swapping using thired variable

---
+++ P5/P5.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n1, n2, c;
+    printf("Enter two number for swapping:\n");
+    scanf("%d%d", &n1, &n2);
+    printf("Before swapping the two numbers are %d and %d.\n", n1, n2);
+    c = n1;
+    n1 = n2;
+    n2 = c;
+    printf("After swapping the two numbers are %d and %d.", n1, n2);
+    return 0;
+}
