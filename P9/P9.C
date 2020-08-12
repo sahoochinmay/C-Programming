1782af8cbcf718522212e061b057b6526fc39094
Chinmay <sahoochinmay54@gmail.com>
2020-08-12 20:55:07 +0530

    Convert temperature into Fahrenheit

---
+++ P9/P9.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    float C, F;
+    printf("Enter temperature in celsius :\n");
+    scanf("%f", &C);
+    F = (1.8 * C) + 32;
+    printf("Temperature in Fahrenheit is %f", F);
+    return 0;
+}
