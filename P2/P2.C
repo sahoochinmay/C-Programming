fa48c1f4c3de6d5d34a14c2aa689a95630704261
Chinmay <sahoochinmay54@gmail.com>
2020-08-20 21:01:58 +0530

    Calculate sum of 2 accepted number .

--- P2/P2.C
+++ P2/P2.C
@@ -1,6 +1,8 @@
-#include<stdio.h>
+// P2. Calculate sum of 2 accepted number .
+#include <stdio.h>
 
-int main() {
+int main()
+{
     int a, b, c = 0;
     printf("Enter two number to calculate sum:\n");
     scanf("%d%d", &a, &b);
