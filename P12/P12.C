1516eebee422d4530249fa6d7be7efff8c4b1649
Chinmay <sahoochinmay54@gmail.com>
2020-08-20 20:51:17 +0530

    Accept a number and check whether positive or negative.

---
+++ P12/P12.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+    if (num >= 0)
+    {
+        printf("Number is positive.");
+    }
+    else
+    {
+        printf("Number is negative.");
+    }
+    return 0;
+}
